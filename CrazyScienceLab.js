df901003b5986543a2bd183f72e7fafb0092a655
DivyaSibin <115955500+Divyaviswan@users.noreply.github.com>
2025-01-25 18:56:02 -0500

    My first commit

---
+++ CrazyScienceLab.js
@@ -0,0 +1,41 @@
+function Participant(pName,age,studyFiled){
+
+return({
+  //Task1 
+    pName,
+    age,
+    studyFiled,
+
+   
+    
+
+//Task2s
+   displayInfo()
+    {
+        console.log("Using Normal function")
+        console.log(`${this.pName},${this.age},${this.studyFiled}`);
+    },
+
+    //TAsk3
+
+  //We can't use shorthand in an Arrow function.
+
+
+});
+
+}
+const  participantObject=new Participant('Divya',12,'Computer');
+participantObject.displayInfo();
+
+
+//Task4
+function updateInfo(pName,propertyValue)
+{
+return(
+{
+    [pName] : propertyValue,
+    [propertyValue] : pName
+    
+});
+}
+
